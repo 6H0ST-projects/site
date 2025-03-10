0a936096d1aa44a270168e88f59aa22a48102515
willgbryan <107731540+willgbryan@users.noreply.github.com>
2025-03-09 23:41:41 -0500

    Update route.ts

--- src/app/api/direct-download/route.ts
+++ src/app/api/direct-download/route.ts
@@ -3,22 +3,22 @@ import { NextRequest, NextResponse } from 'next/server';
 // Map of file keys to their direct download URLs
 const fileMap: Record<string, { url: string, filename: string, contentType: string }> = {
   'mac-dmg': { 
-    url: 'https://gqzkgvpzdiquahcv.public.blob.vercel-storage.com/reach-1.0.0-arm64-EDHj9UbOToabdc5vKyYyg0F1BvTF3U.dmg',
-    filename: 'reach-1.0.0-arm64.dmg',
+    url: 'https://gqzkgvpzdiquahcv.public.blob.vercel-storage.com/Reach-1.0.0-arm64-HYNSGhpHAUy2zZROGHjWQTmWSswny4.dmg',
+    filename: 'Reach-1.0.0-arm64.dmg',
     contentType: 'application/octet-stream'
   },
   'mac-zip': { 
-    url: 'https://gqzkgvpzdiquahcv.public.blob.vercel-storage.com/reach-1.0.0-arm64-mtTGSJxJB97T5AZ1CsJUM6UhVO7Wru.zip',
-    filename: 'reach-1.0.0-arm64.zip',
+    url: 'https://gqzkgvpzdiquahcv.public.blob.vercel-storage.com/Reach-1.0.0-x64-TPVLMBDicjw6z7BvMbH2pfxU4F8meu.dmg',
+    filename: 'Reach-1.0.0-x64.dmg',
     contentType: 'application/zip'
   },
   'windows': { 
-    url: 'https://gqzkgvpzdiquahcv.public.blob.vercel-storage.com/reach-1.0.0-setup-9vFuLBTwBhST1kHFZ1cR74Jt5Us6fc.exe',
+    url: 'https://gqzkgvpzdiquahcv.public.blob.vercel-storage.com/reach-1.0.0-setup-f6YbwihNtvLTkTW0A1olRo7LCzXQjx.exe',
     filename: 'reach-1.0.0-setup.exe',
     contentType: 'application/octet-stream'
   },
   'windows-portable': { 
-    url: 'https://gqzkgvpzdiquahcv.public.blob.vercel-storage.com/reach-1.0.0-portable-Gl8kWXhPfc6VXS5ksR3Mrcm9AkpDz6.exe',
+    url: 'https://gqzkgvpzdiquahcv.public.blob.vercel-storage.com/reach-1.0.0-portable-iGgwTohRXFzoEbTnzDupdLAeChbwgc.exe',
     filename: 'reach-1.0.0-portable.exe',
     contentType: 'application/octet-stream'
   }
